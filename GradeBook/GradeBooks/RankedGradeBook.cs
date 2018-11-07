83daa23fbb30da0c38373ca32ba1a518764a4d55
Micheal Ashley <micheal.ashley@macefusion.com>
2018-11-07 14:06:12 -0500

    Ranked Grade Calculate Statistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -6,21 +6,19 @@ namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
-        readonly int numberOfStudents;
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
-            numberOfStudents = Students.Count;
         }
 
         public override char GetLetterGrade(double averageGrade)
         {
-            if (numberOfStudents < 5)
+            if (Students.Count < 5)
             {
                 throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work." );
             }
 
-            int fifth = (int)Math.Floor(numberOfStudents * 0.2);
+            int fifth = (int)Math.Floor(Students.Count * 0.2);
             var gradeOrder = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
 
             if (gradeOrder[fifth - 1] <= averageGrade)
@@ -36,7 +34,7 @@ namespace GradeBook.GradeBooks
 
         public override void CalculateStatistics()
         {
-            if (numberOfStudents < 5)
+            if (Students.Count < 5)
             {
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
                 return;
@@ -47,7 +45,7 @@ namespace GradeBook.GradeBooks
 
         public override void CalculateStudentStatistics(string name)
         {
-            if (numberOfStudents < 5)
+            if (Students.Count < 5)
             {
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
                 return;
