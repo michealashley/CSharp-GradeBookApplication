63d023edd4e203ec68951be9cf60b501d59b811a
Micheal Ashley <micheal.ashley@macefusion.com>
2018-11-07 13:53:23 -0500

    Ranked Grade Letter Grade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,6 @@
 ﻿using GradeBook.Enums;
+using System;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -7,6 +9,28 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            int numberOfStudents = Students.Count;
+            if (numberOfStudents < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work." );
+            }
+
+            int fifth = (int)Math.Floor(numberOfStudents * 0.2);
+            var gradeOrder = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (gradeOrder[fifth - 1] <= averageGrade)
+                return 'A';
+            else if(gradeOrder[(fifth * 2) - 1] <= averageGrade)
+                return 'B';
+            else if (gradeOrder[(fifth * 3) - 1] <= averageGrade)
+                return 'C';
+            else if (gradeOrder[(fifth * 4) - 1] <= averageGrade)
+                return 'D';
+            else return 'F';
         }
     }
 }
