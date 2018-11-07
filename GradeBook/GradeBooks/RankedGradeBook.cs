92569b57b8859625a7419756a67377684d51517c
Micheal Ashley <micheal.ashley@macefusion.com>
2018-11-07 14:03:01 -0500

    Ranked Grade Calculate Statistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -40,10 +40,8 @@ namespace GradeBook.GradeBooks
             {
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
                 return;
-            } else
-            {
-                base.CalculateStatistics();
             }
+            base.CalculateStatistics();
             
         }
 
@@ -54,8 +52,7 @@ namespace GradeBook.GradeBooks
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
                 return;
             }
-            else
-                base.CalculateStudentStatistics(name);
+            base.CalculateStudentStatistics(name);
         }
     }
 }
